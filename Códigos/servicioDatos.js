7fb84faaf461ec8f91702334ea287c59c05ded9d
Alan <alan.11.gt@gmail.com>
2024-02-20 03:00:25 -0800

    Versión final

--- Códigos/servicioDatos.js
+++ Códigos/servicioDatos.js
@@ -1,13 +1,13 @@
 let finDeTransmisionDeDatos = function(id){
     console.log('Transferencia', id, 'terminada');
-    return Promise.resolve(); 
+    return Promise.resolve(); // Resolvemos la Promise una vez que se ha completado la transferencia
 }
 
 let obtenDatosDeInternet = function (id, duracion) {
     console.log('Proceso', id, 'obteniendo datos de internet');
     return new Promise(resolve => {
         setTimeout(() => {
-            resolve(finDeTransmisionDeDatos(id)); 
+            resolve(finDeTransmisionDeDatos(id)); // Resolvemos la Promise una vez que se ha completado la transferencia
         }, duracion);
     });
 }
